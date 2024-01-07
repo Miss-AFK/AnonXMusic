ddf227b3d0f86d07e326f37db3869b5636ef6636
ᴍɪss ᴀғᴋ <147545354+Miss-AFK@users.noreply.github.com>
2024-01-07 20:20:50 +0530

    Add files via upload

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -1,5 +1,7 @@
 import math
 
+import config
+
 from pyrogram.types import InlineKeyboardButton
 
 from AnonXMusic.utils.formatters import time_to_seconds
@@ -33,26 +35,32 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "─▷─────────"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "──▷────────"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "───▷───────"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "────▷──────"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "─────▷─────"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "──────▷────"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "───────▷───"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "────────▷──"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "─────────▷─"
     else:
-        bar = "—————————◉"
+        bar = "──────────▷"
     buttons = [
+        [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
+        ],
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
@@ -62,8 +70,10 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
+                text="ᴜᴘᴅᴀᴛᴇs 📢", url=config.SUPPORT_CHANNEL
+            ),
+            InlineKeyboardButton(
+                text="sᴜᴘᴘᴏʀᴛ 💬", url=config.SUPPORT_CHAT
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -96,6 +106,14 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
                 text=_["P_B_2"],
                 callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="ᴜᴘᴅᴀᴛᴇs 📢", url=config.SUPPORT_CHANNEL
+            ),
+            InlineKeyboardButton(
+                text="sᴜᴘᴘᴏʀᴛ 💬", url=config.SUPPORT_CHAT
+            )
         ],
         [
             InlineKeyboardButton(
