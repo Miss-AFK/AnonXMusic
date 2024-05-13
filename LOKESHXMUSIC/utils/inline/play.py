01e89122d14d76338ab9197769f14a4947371f89
ᴍɪss ᴀғᴋ <147545354+Miss-AFK@users.noreply.github.com>
2024-05-13 21:51:59 +0530

    Update play.py

--- LOKESHXMUSIC/utils/inline/play.py
+++ LOKESHXMUSIC/utils/inline/play.py
@@ -79,14 +79,52 @@ def stream_markup_timer(_, chat_id, played, dur):
     return buttons
 
 
-def stream_markup(_, chat_id):
+def stream_markup_timer(_, chat_id, played, dur):
+    played_sec = time_to_seconds(played)
+    duration_sec = time_to_seconds(dur)
+    percentage = (played_sec / duration_sec) * 100
+    umm = math.floor(percentage)
+    if 0 < umm <= 10:
+        bar = "─▷─────────"
+    elif 10 < umm < 20:
+        bar = "──▷────────"
+    elif 20 <= umm < 30:
+        bar = "───▷───────"
+    elif 30 <= umm < 40:
+        bar = "────▷──────"
+    elif 40 <= umm < 50:
+        bar = "─────▷─────"
+    elif 50 <= umm < 60:
+        bar = "──────▷────"
+    elif 60 <= umm < 70:
+        bar = "───────▷───"
+    elif 70 <= umm < 80:
+        bar = "────────▷──"
+    elif 80 <= umm < 95:
+        bar = "─────────▷─"
+    else:
+        bar = "──────────▷"
     buttons = [
+        [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
+        ],
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [
+            InlineKeyboardButton(
+                text="ᴜᴘᴅᴀᴛᴇs 📢", url=config.SUPPORT_CHANNEL
+            ),
+            InlineKeyboardButton(
+                text="sᴜᴘᴘᴏʀᴛ 💬", url=config.SUPPORT_CHAT
+            )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
